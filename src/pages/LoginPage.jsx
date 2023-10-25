c9cff227efb5680f1d81a4cda40bbfd9b05cae22
dima <d.oliynyk@outlook.com>
2023-10-25 15:25:59 +0300

    added simple login form

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,10 +1,39 @@
 import React, { Component } from 'react';
 
 export default class LoginPage extends Component{
+    state = {
+        username: "",
+        password: ""
+    }
+
+    constructor(p) {
+        super(p);
+        this.sendUser = this.sendUser.bind(this);
+    } 
+
+    handleUser = (e) => {
+        if(e.target.id === 'username'){
+            this.setState({ username: e.target.value })
+        } else if(e.target.id === 'password'){
+            this.setState({ password: e.target.value })
+        }
+        // console.log(e.target.value)
+    }
+
+    sendUser(){
+        console.log(this.state)
+    }
     render(){
         return(
             <>
                 <h2>Login Page</h2>
+
+                <form>
+                    <input type="text" placeholder="enter your username" id="username" onChange={this.handleUser}/>
+                    <input type="text" placeholder="enter your password" id="password" onChange={this.handleUser}/>
+                </form>
+                <button onClick={this.sendUser}>submit</button>
+
             </>
         )
     }
