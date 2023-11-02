6eb3ad1c8c60a087ef17df5b32ae7c322fd82507
dima <d.oliynyk@outlook.com>
2023-11-02 16:24:46 +0300

    fix loginization

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 import React, { Component } from 'react';
 import { Routes, Route, Link } from 'react-router-dom'
 
-
 import LoginPage from './pages/LoginPage.jsx';
 import HomePage from './pages/HomePage.jsx';
+import ProfilePage from './pages/ProfilePage.jsx';
 
 export default class App extends Component{
   render(){
@@ -12,6 +12,7 @@ export default class App extends Component{
         <Routes>
           <Route exact path="/" element={<HomePage/>} />
           <Route path="/login" element={<LoginPage/>} />
+          <Route path="/profile/:username" element={<ProfilePage/>} />
         </Routes>
           
   
